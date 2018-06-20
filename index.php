9082d553bee65afb72fadade782e94b509c5d107
Frank E. Cafiel <fcafiel@dayscript.com>
2018-06-20 10:12:23 -0500

    Test git

--- index.php
+++ index.php
@@ -11,7 +11,8 @@
 	<link rel="stylesheet" type="text/css" href="includes/styles.css">
 </head>
 <body>
-	<iframe id="typeform-full" width="100%" height="100%" frameborder="0" src="https://soportecolombia.typeform.com/to/hJyP2s"></iframe>
-	<script type="text/javascript" src="https://embed.typeform.com/embed.js"></script>
+	<h1>Trivia Uber Allianz</h1>
+	<!-- <iframe id="typeform-full" width="100%" height="100%" frameborder="0" src="https://soportecolombia.typeform.com/to/hJyP2s"></iframe>
+	<script type="text/javascript" src="https://embed.typeform.com/embed.js"></script> -->
 </body>
 </html>
